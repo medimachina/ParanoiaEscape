8f25f51d97f3bee4b7b222784427ad0b315c5c8c
Per Johansson <veraksar@users.noreply.github.com>
2022-02-24 23:27:55 +0100

    Pejo/sound fx (#8)
    * add walking and door audio

---
+++ Assets/0_Script/StringAudioPair.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+[System.Serializable]
+public struct StringAudioPair
+{
+    [HorizontalGroup("pair"), LabelWidth(30)]
+    public string key;
+    [HorizontalGroup("pair"), LabelWidth(30)]
+    public AudioClip clip;
+}
